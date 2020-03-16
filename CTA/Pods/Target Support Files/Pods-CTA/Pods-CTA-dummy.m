186e50e27cc595d9661aa837125073ea18869122
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-03-16 12:00:19 -0400

    updated

---
+++ CTA/Pods/Target Support Files/Pods-CTA/Pods-CTA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CTA : NSObject
+@end
+@implementation PodsDummy_Pods_CTA
+@end
---
+++ CTA/Pods/Target Support Files/Pods-CTATests/Pods-CTATests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CTATests : NSObject
+@end
+@implementation PodsDummy_Pods_CTATests
+@end
