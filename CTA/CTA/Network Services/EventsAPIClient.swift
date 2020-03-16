7fb0a0712e59ee909187b30c4e6552fa0767be72
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-03-16 18:38:06 -0400

    added files

---
+++ CTA/CTA/Network Services/EventsAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  EventsAPIClient.swift
+//  CTA
+//
+//  Created by Oscar Victoria Gonzalez  on 3/16/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import Foundation
