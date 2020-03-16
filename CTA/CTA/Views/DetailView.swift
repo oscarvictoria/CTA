dfd51e6a2aa34c3e94b532b085a78ac57449314e
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-03-16 18:47:53 -0400

    updated files

---
+++ CTA/CTA/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  CTA
+//
+//  Created by Oscar Victoria Gonzalez  on 3/16/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
